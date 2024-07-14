547fb51e3948fd468e04fd0352d05d19b97c1932
mymotherati <100pofarengeitu@gmail.com>
2024-07-14 12:37:55 +0300

    Changes in UI in main.py, added readme

---
+++ README.md
@@ -0,0 +1,21 @@
+## Asynchronous automation WooFi protocol in Arbitrum network
+_____________________________________________
+This script will help you, if you need to transfer or stake tokens in multiple wallets very fast.
+It only works in Arbitrum network for now.
+
+Python version - **3.10.4**
+### Functions:
+- Swap **$ETH** to **$USDC**
+- Swap **$USDC** to **$ETH**
+- Swap **$ETH** to **$WOO**
+- Cross-chain swap Arbitrum **$ETH** to Polygon **$ETH**
+- Stake **$WOO**
+- Unstake **$WOO**
+
+All functions you can find in tasks/woofi.py
+
+### How to run?
+1. Create virtual environment and install all requirements
+2. Add wallets.txt in data directory and put your private keys there
+3. In main.py set amount and needed operations to do and follow instructions there
+4. Run main.py
--- main.py
+++ main.py
@@ -14,29 +14,45 @@ async def load_wallets():
     async with aiofiles.open("./data/wallets.txt", 'r') as file:
         return [Account.from_key(line.replace("\n", "")) for line in (await file.readlines())]
 
-async def swap(woofi, amount):
-    await woofi.swap_eth_to_woo(amount=amount)
+async def swap_eth_woo(woofi, amount):
+    to_amount = TokenAmount(amount)
+    await woofi.swap_eth_to_woo(amount=to_amount)
+
+async def swap_eth_usdc(woofi, amount):
+    to_amount = TokenAmount(amount)
+    await woofi.swap_eth_to_usdc(amount=to_amount)
+
+async def swap_usdc_eth(woofi, amount):
+    to_amount = TokenAmount(amount, decimals=6)
+    await woofi.swap_usdc_to_eth(amount=to_amount)
+
+async def cross_chain(woofi, amount):
+    to_amount = TokenAmount(amount)
+    await woofi.eth_cross_chain_swap(amount=to_amount)
 
 async def stake(woofi, amount):
-    await woofi.stake_woo(amount=amount)
+    to_amount = TokenAmount(amount)
+    await woofi.stake_woo(amount=to_amount)
 
 async def unstake(woofi, amount):
-    await woofi.unstake_woo(amount=amount)
+    to_amount = TokenAmount(amount)
+    await woofi.unstake_woo(amount=to_amount)
 
 async def main():
     wallets = await load_wallets()
-    amount_eth = TokenAmount(0.0001)
-    amount_woo = TokenAmount(0.1)
+    amount = 0.1 # put your amount here
 
     tasks = []
     for wallet in wallets:
         client = Client(wallet=wallet, network=Arbitrum)
         woofi = WooFi(client=client)
+        tasks.append(asyncio.create_task(swap(woofi=woofi, amount=amount_eth))) 
+        # duplicate this string, if you need to do multiple operations in 1 wallet
+        
+        # specify the desired operation name by specifying the functions that are specified above(swap_eth_woo, etc.)..
+        # ..instead of "swap" in these strings
         tasks.append(asyncio.create_task(swap(woofi=woofi, amount=amount_eth)))
-
-    # client = Client(wallet=wallets[1], network=Arbitrum)
-    # woofi = WooFi(client=client)
-    # tasks.append(asyncio.create_task(stake(woofi=woofi, amount=amount_woo)))
+        tasks.append(asyncio.create_task(swap(woofi=woofi, amount=amount_eth)))
 
     for completed_task in asyncio.as_completed(tasks):
         try:
